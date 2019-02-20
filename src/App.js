c8747c1e95f0776e7092b9582129cae13e1f1a3c
saniv <venkatareddy.sanikommu@pega.com>
2019-02-20 14:35:14 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Routes } from './Routes';
+import { Routes } from 'Routes';
 import { Provider } from 'react-redux';
 import { configureStore } from './store/configureStore'
 const store = configureStore();
